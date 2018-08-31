fd600b3255e3a8538f621f253bd3d5e6a16af5f6
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2018-08-31 16:11:24 +0700

    finish captcha unit test with random all parts

--- src/captcha.js
+++ src/captcha.js
@@ -1,28 +1,23 @@
 const random = require('./random')
-const result = [
-  {
-    captcha: '1 + 1',
-    answer: '2'
-  },
-  {
-    captcha: '2 + 1',
-    answer: '3'
-  },
-  {
-    captcha: '2 * 1',
-    answer: '2'
-  },
-  {
-    captcha: '2 * 5',
-    answer: '10'
-  }
-]
 
 module.exports = {
-  random () {
-    return random.range(0, 3)
+  randomFirstNumber () {
+    return random.range(0, 100)
+  },
+  randomSecondNumber () {
+    return random.range(0, 100)
+  },
+  randomOperator () {
+    return ['+', '-', '*', '/'][random.range(0, 3)]
   },
   generate () {
-    return result[this.random()]
+    let firstNumber = this.randomFirstNumber()
+    let secondNumber = this.randomSecondNumber()
+    let operator = this.randomOperator()
+    let captcha = `${firstNumber} ${operator} ${secondNumber}`
+    return {
+      captcha,
+      answer: eval(captcha).toString()
+    }
   }
 }
--- tests/unit/captcha.spec.js
+++ tests/unit/captcha.spec.js
@@ -1,8 +1,10 @@
 const captcha = require('../../src/captcha')
 
 describe('captcha', function () {
-  test('should return captcha', function () {
-    captcha.random = jest.fn().mockReturnValue(0)
-    expect(captcha.generate()).toEqual({ captcha: '1 + 1', answer: '2' })
+  test(`should generate '10 + 20 = 30'`, function () {
+    captcha.randomFirstNumber = jest.fn().mockReturnValue(10)
+    captcha.randomSecondNumber = jest.fn().mockReturnValue(20)
+    captcha.randomOperator = jest.fn().mockReturnValue('+')
+    expect(captcha.generate()).toEqual({ captcha: '10 + 20', answer: '30' })
   })
 })
