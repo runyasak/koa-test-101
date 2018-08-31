12669ad46da4fdf0126446078ec5c0811b3c9525
aunn <aunn_it08@hotmail.com>
2018-08-31 13:20:19 +0700

    add mongoose and product model

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 const koaBody = require('koa-body')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
 
 const server = new Koa()
 const router = new Router()
@@ -9,11 +11,11 @@ router.post('/product/stock', async (ctx, next) => {
   ctx.body = 'AAA'
 })
 
-router.get('/test', async (ctx, next) => {
+router.get('/captcha', async (ctx, next) => {
   ctx.body = '33445566'
 })
 
-router.get('/eeee', async (ctx, next) => {
+router.get('/random', async (ctx, next) => {
   ctx.body = '1111'
 })
 
