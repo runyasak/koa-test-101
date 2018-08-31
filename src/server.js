7003cc15b3b74bbe705ae7beb699ac8bb8152f0c
aunn <aunn_it08@hotmail.com>
2018-08-31 11:42:16 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const koaBody = require('koa-body')
+
+const server = new Koa()
+const router = new Router()
+
+router.post('/product/stock', async (ctx, next) => {
+    
+})
+
+router.get('/test', async (ctx, next) => {
+    ctx.body = '33445566'
+})
+
+router.get('/eeee', async (ctx, next) => {
+    ctx.body = '1111'
+})
+
+server
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+module.exports = server
